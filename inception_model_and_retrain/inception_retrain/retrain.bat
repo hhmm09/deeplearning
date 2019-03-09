d7b77520878ecb617eea0eafe92b7356e2d5e800
marshen <1179160244@outlook.com>
2019-03-09 12:21:40 +0800

    google inception module
    you can use it to train or retrain the image recognize

---
+++ inception_model_and_retrain/inception_retrain/retrain.bat
@@ -0,0 +1,8 @@
+python C:/Users/Mars/Desktop/wokstation/inception_retrain/inception_retrain.py ^
+--bottleneck_dir bottleneck ^
+--how_many_training_steps 200 ^
+--model_dir C:/Users/Mars/Desktop/wokstation/inception_model/ ^
+--output_graph output_graph.pb ^
+--output_labels output_labels.txt ^
+--image_dir C:/Users/Mars/Desktop/wokstation/inception_retrain/data/train/
+pause
